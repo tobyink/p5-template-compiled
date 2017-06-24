129ddbed10292b4102b201a5d22f8121060a8493
Toby Inkster <mail@tobyinkster.co.uk>
2017-06-24 12:48:23 +0100

    bump version; changelog

--- lib/Template/Compiled.pm
+++ lib/Template/Compiled.pm
@@ -5,7 +5,7 @@ use warnings;
 package Template::Compiled;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.001';
+our $VERSION   = '0.002';
 
 use Eval::TypeTiny qw( eval_closure );
 use Types::Standard qw( -types slurpy Join );
--- lib/Template/Compiled/Utils.pm
+++ lib/Template/Compiled/Utils.pm
@@ -5,7 +5,7 @@ use warnings;
 package Template::Compiled::Utils;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.001';
+our $VERSION   = '0.002';
 
 use Exporter::Shiny our @EXPORT = qw(
 	echo
