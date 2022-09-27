bc8fe02f635a28f8597b0b62c231c66e710ec70c
Toby Inkster <mail@tobyinkster.co.uk>
2022-09-27 14:37:21 +0100

    bump version; changelog

--- lib/Template/Compiled.pm
+++ lib/Template/Compiled.pm
@@ -5,7 +5,7 @@ use warnings;
 package Template::Compiled;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.002';
+our $VERSION   = '0.003';
 
 use Eval::TypeTiny::CodeAccumulator;
 use Types::Standard qw( -types Join );
